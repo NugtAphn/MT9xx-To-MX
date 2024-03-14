895e597933e0a1b068600f4c15039cff3822225f
Edward Phan <edward.phan.work@gmail.com>
2024-03-14 16:27:44 +0700

    build splitBlock

--- convert/mt910/SplitBlock.py
+++ convert/mt910/SplitBlock.py
@@ -10,12 +10,13 @@ def splitBlock1(line):
             block1.append(i[:])
     appID = block1[0][2:3]
     serviceID = block1[0][3:5]
-    BIC = block1[0][5:17]
+    BIC11 = block1[0][5:17]
     sessionDigit = block1[0][17:21]
     sequenceDigit = block1[0][21:27]
+    print(len(block1))
     print('1. Application ID: ' + appID + '\n' +
           '2. Service ID: ' + serviceID + '\n' +
-          '3. Business Identify Code: ' + BIC + '\n' +
+          '3. Business Identify Code: ' + BIC11 + '\n' +
           '4. Session Digit: ' + sessionDigit + '\n' +
           '5. Sequence Digit: ' + sequenceDigit)
 
@@ -28,15 +29,26 @@ def splitBlock2(line):
         if i[0:2] == '2:':
             block2.append(i[:])
 
-    io = block2[0][2:3]
-    mt = block2[0][3:6]
-
-    BIC = ''
-    for i in block2[0][6:-1]:
-        if i.isalpha():
-            BIC += i
-
+    appHeader = block2[0][2:3]
+    msgType = block2[0][3:6]
+    inTime = block2[0][3:6]
+    inDate = block2[0][3:6]
+    BIC11 = block2[0][3:6]
+    sessionDigit = block2[0][17:21]
+    sequenceDigit = block2[0][21:27]
+    outTime = block2[0][3:6]
+    outDate = block2[0][3:6]
     priority = block2[0][-1:]
+    print('1. Application Header ID: ' + appHeader + '\n' +
+          '2. Message Type: ' + msgType + '\n' +
+          '3. Input Time: ' + inTime + '\n' +
+          '4. Input Date: ' + inDate + '\n' +
+          '5. Business Identify Code: ' + BIC11 + '\n' +
+          '6. Session Digit: ' + sessionDigit + '\n' +
+          '7. Sequence Digit: ' + sequenceDigit + '\n' +
+          '8. Output Time: ' + outTime + '\n' +
+          '9. Output Date: ' + outDate + '\n' +
+          '10. Priority Code: ' + priority)
 
 
 def splitBlock4(line):
